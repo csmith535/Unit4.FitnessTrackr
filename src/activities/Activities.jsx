190d11b1a75a48abe06b647fedbbf4b994d8fb5b
Cameron Smith <cameron.smith3030@gmail.com>
2025-09-17 22:12:31 -0400

    add button to DELETE req for unique activity id, only appears while logged in, error if not authorized to delete

--- src/activities/Activities.jsx
+++ src/activities/Activities.jsx
@@ -1,19 +1,61 @@
 import useQuery from "../api/useQuery";
+import { useApi } from "../api/ApiContext";
+import { useAuth } from "../auth/AuthContext";
+import { useState } from "react";
 
 export default function Activities() {
-  const { data: activities, loading, error } = useQuery("/activities");
+  const {
+    data: activities,
+    loading,
+    error,
+  } = useQuery("/activities", "activities");
+  const { token } = useAuth();
+  const { request, invalidateTags } = useApi();
+  const [deleteError, setDeleteError] = useState(null);
+  const [errorActivityId, setErrorActivityId] = useState(null);
+  const [deleting, setDeleting] = useState(false);
+
+  const handleDelete = async (id) => {
+    setDeleteError(null);
+    setErrorActivityId(null);
+    setDeleting(true);
+    try {
+      await request(`/activities/${id}`, {
+        method: "DELETE",
+      });
+      invalidateTags(["activities"]);
+    } catch (e) {
+      setDeleteError(`Failed to delete activity: ${e.message}`);
+      setErrorActivityId(id);
+    } finally {
+      setDeleting(false);
+    }
+  };
 
   if (loading) {
     return <div>Loading Activities</div>;
   } else if (error) {
     return <div>{error}</div>;
-  } else if (activities || loading) {
+  } else if (activities) {
     return (
       <div>
         {activities.map((data) => (
-          <div key={data.id}>
+          <div key={data.id} className="activity-card">
             <h3>{data.name}</h3>
             <p>{data.description}</p>
+            {token ? (
+              <>
+                <button
+                  onClick={() => handleDelete(data.id)}
+                  disabled={deleting}
+                >
+                  Remove Activity
+                </button>
+                {deleteError && errorActivityId === data.id && (
+                  <div style={{ color: "red" }}>{deleteError}</div>
+                )}
+              </>
+            ) : null}
           </div>
         ))}
       </div>
--- src/activities/ActivitiesPage.jsx
+++ src/activities/ActivitiesPage.jsx
@@ -1,11 +1,22 @@
 import Activities from "./Activities";
+import { useAuth } from "../auth/AuthContext";
 
 export default function ActivitiesPage() {
+  const { token } = useAuth();
   return (
     <>
       <h1>Activities</h1>
       <p>Imagine all the activities!</p>
-      <Activities />
+      {token ? (
+        <>
+          <Activities />
+          <form>
+            <label>Add Activity</label>
+          </form>
+        </>
+      ) : (
+        <Activities />
+      )}
     </>
   );
 }
