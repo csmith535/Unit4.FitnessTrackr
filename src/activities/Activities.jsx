55894746e210beae2dbaa6c45edc72f499c086c9
Cameron Smith <cameron.smith3030@gmail.com>
2025-09-22 14:12:11 -0400

    separate activity into list and list item, can finally use mutate easily

--- src/activities/Activities.jsx
+++ src/activities/Activities.jsx
@@ -1,8 +1,6 @@
 import useQuery from "../api/useQuery";
 import useMutation from "../api/useMutation";
-import { useApi } from "../api/ApiContext";
 import { useAuth } from "../auth/AuthContext";
-import { useState } from "react";
 
 export default function Activities() {
   const {
@@ -10,31 +8,6 @@ export default function Activities() {
     loading,
     error,
   } = useQuery("/activities", "activities");
-  const { token } = useAuth();
-  const { invalidateTags } = useApi();
-  const [deleteError, setDeleteError] = useState(null);
-  const [deleting, setDeleting] = useState(false);
-  const [deleteID, setDeleteID] = useState(1);
-  const { mutate: deleteReq } = useMutation(
-    "DELETE",
-    deleteID ? `/activities/${deleteID}` : "/activities/1",
-    ["activities"]
-  );
-
-  const handleDelete = async (id) => {
-    setDeleteError(null);
-    setDeleting(true);
-    setDeleteID(id);
-
-    try {
-      await deleteReq();
-      invalidateTags(["activities"]);
-    } catch (e) {
-      setDeleteError(`Failed to delete activity: ${e.message}`);
-    } finally {
-      setDeleting(false);
-    }
-  };
 
   if (loading) {
     return <div>Loading Activities</div>;
@@ -42,21 +15,31 @@ export default function Activities() {
     return <div>{error}</div>;
   } else if (activities) {
     return (
-      <div>
-        {activities.map((data) => (
-          <div key={data.id} className="activity-card">
-            <h3>{data.name}</h3>
-            <p>{data.description}</p>
-            {token ? (
-              <button onClick={() => handleDelete(data.id)} disabled={deleting}>
-                Remove Activity
-              </button>
-            ) : null}
-          </div>
+      <ul>
+        {activities.map((activity) => (
+          <ActivityItem key={activity.id} activity={activity} />
         ))}
-
-        <div>{deleteError}</div>
-      </div>
+      </ul>
     );
   }
+}
+
+function ActivityItem({ activity }) {
+  const { token } = useAuth();
+  const {
+    mutate: deleteReq,
+    loading: mutateLoad,
+    error: mutateError,
+  } = useMutation("DELETE", `/activities/` + activity.id, ["activities"]);
+
+  return (
+    <li>
+      <p>{activity.name}</p>
+      {token && (
+        <button onClick={() => deleteReq()}>
+          {mutateLoad ? "Deleting..." : mutateError ? mutateError : "Delete"}
+        </button>
+      )}
+    </li>
+  );
 }
